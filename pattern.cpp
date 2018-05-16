6681ee0d629f469528ef05e35d5fc4b0f0518175
sharco-code <38721575+sharco-code@users.noreply.github.com>
2018-05-16 22:28:01 +0200

    xd

--- pattern.cpp
+++ pattern.cpp
@@ -1,46 +1,61 @@
 #include <iostream>
+#include <chrono>
+#include <random>
+#include <windows.h>
 
 class patterngen {
 public:
-    patterngen();
-    void seed(int a, int b);
+    patterngen(int a, int b, int c,int d);
     void fill();
     void print();
 
-
-    bool wtfx;
-    std::string wtf;
-
 private:
-    int rule_1_max = 45;
-    int rule_1_count;
+    int rule_1_max = 120;
+    int rule_1_count = 0;
+    int rule_4_max = 3;
+    int rule_4_count = 0;
+
     int height = 20, width = 40;
     bool value = false;
     bool arr[20][40];
 };
 ///////////////////////////////////////////////////////////////////
-patterngen::patterngen() {
+patterngen::patterngen(int a, int b, int c,int d) {
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
             arr[i][j] = false;
         }
     }
-}
-void patterngen::seed(int a, int b) {
+    rule_1_max = c;
+    rule_4_max = d;
     arr[a][b] = true;
 }
+
 void patterngen::fill() {
     for (int i = 0; i < height; ++i) {          
         for (int j = 0; j < width; ++j) {
-            if(rule_1_count <= rule_1_max) {                                                //rule 1
 
-                if(j != 0 && i != 0 && j != 39 && i != 19) {                                //rule 2
+             //rule 1 - SOLO SE PUEDEN IMPRIMIR HASTA UN NUMERO DETERMINADO
+            if(rule_1_count <= rule_1_max) {     
 
-                    if(arr[i-1][j] || arr[i+1][j] || arr[i][j-1] || arr[i][j+1]) {          //rule 3
-                        value = true;
-                        rule_1_count++;
-                        arr[i][j] = value;
-                        wtfx = arr[i-1][j];
+                //rule 2 - SOLO SE IMPRIME SI NO ESTA EN UNA DE CASLLA LATERAL
+                if(j != 0 && i != 0 && j != 39 && i != 19) {                                
+
+                    //rule 3 - SOLO IMPRIME SI TIENE ALGUNO A SU ALREDEDOR
+                    //    TOP             BOT           LEFT           RIGHT         LEFT-TOP         LEFT-BOT        RIGHT-TOP       RIGHT-BOT
+                    if(arr[i-1][j] || arr[i+1][j] || arr[i][j-1] || arr[i][j+1] || arr[i-1][j-1] || arr[i+1][j-1] || arr[i-1][j+1] || arr[i+1][j+1]) {
+                    
+                        //rule 4 -  SOLO PUEDE IMPRIMIR LINEAS HASTA UN NUMERO DETERMINADO
+                        if(rule_4_count < rule_4_max) {
+
+    value = true;
+    rule_1_count++;
+    rule_4_count++;
+    arr[i][j] = value;
+
+                        } else if (rule_4_count == rule_4_max) {
+                            rule_4_count = 0;
+                        }
                     }
                 }
             }
@@ -57,11 +72,25 @@ void patterngen::print() {
 } 
 ////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
-    patterngen pattern;
-    pattern.seed(10,8);
-    pattern.fill();
-    pattern.print();
-    std::cout << pattern.wtfx;
+    int a, b, c, d;
+    std::default_random_engine generator;
+    auto generar_entre = [&generator](int min, int max) -> int {
+        std::uniform_int_distribution<int> d(min, max);
+        return d(generator);
+    };
+
+    std::cout << a <<" "<< b <<" "<< c <<" "<< d << std::endl;
+    system("PAUSE");
+    while(true) {
+        system("CLS");
+        a = generar_entre(2,18);
+        b = generar_entre(2,40);
+        c = generar_entre(50,700);
+        d = generar_entre(3,9);
+        patterngen pattern(a,b,c,d);
+        pattern.fill();
+        pattern.print(); 
+    }
     return 0;
 }
 
